cd8acb0f8a7b9448c1d65af01c0db33ebb64333c
Kiyah <132413293+Kxyah13@users.noreply.github.com>
2026-04-05 20:20:26 +1000

    Enemy + Bullet Speeds Updated
    The bullets had gravity to them + were not being shot far enough to collide with asteroids, so parameters have been altered.
    
    Asteroids have also changed in speed as they were going too fast
    
    Comments and indents have been changed on these scripts to a more cleaner style

---
+++ KiyahDomogala_Task1BSpace/Assets/SGD213_Task1B-ModularSpaceShooter-main/SGD213_Task1B-ModularSpaceShooter-main/Assets/Scripts/BulletMoveForward.cs
@@ -0,0 +1,29 @@
+﻿
+//BULLET MOVEFORWARD
+
+using UnityEngine;
+using System.Collections;
+
+public class BulletMoveForward : MonoBehaviour {
+
+//Speed and velocity of the bullets
+private float acceleration = 50f;
+private float initialVelocity = 50f;
+private Rigidbody2D ourRigidbody;
+
+  // Use this for initialization
+  void Start()
+   {
+     ourRigidbody = GetComponent<Rigidbody2D>();
+
+     ourRigidbody.velocity = Vector2.up * initialVelocity;
+   }
+
+  // Update is called once per frame
+  void Update()
+   {
+      Vector2 ForceToAdd = Vector2.up * acceleration * Time.deltaTime;
+
+      ourRigidbody.AddForce(ForceToAdd);
+   }
+}
