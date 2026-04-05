10d3c2d7f3c4199e7e4710d233e74807eb8005b0
Kiyah <132413293+Kxyah13@users.noreply.github.com>
2026-04-05 22:42:05 +1000

    Movement Update
    Movement Script has been added in for the following three:
    PLAYER
    BULLETS
    ENEMY (ROCKS)
    
    All parts call to their respective scripts

--- KiyahDomogala_Task1BSpace/Assets/SGD213_Task1B-ModularSpaceShooter-main/SGD213_Task1B-ModularSpaceShooter-main/Assets/Scripts/Movement.cs
+++ KiyahDomogala_Task1BSpace/Assets/SGD213_Task1B-ModularSpaceShooter-main/SGD213_Task1B-ModularSpaceShooter-main/Assets/Scripts/Movement.cs
@@ -5,6 +5,66 @@ using UnityEngine;
 public enum DirectionList
 {
     PlayerDirection
-        BulletDirection
-        RockDirection
+    BulletDirection
+    EnemyDirection
 }
+
+public class Movement : MonoBehaviour
+{
+    //List to know what object needs
+    [SerializeField]
+    private DirectionList directionList - DirectionList.PlayerDirection;
+    private Rigidbody2D rb;
+
+    //Set values to 0 Zero
+    private float acceleration = 0f;
+    private float initialVelocity = 0f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //Rock initialVelocity and acceleration
+        if (directionList == DirectionList.EnemyDirection)
+        {
+            acceleration = 75f;
+            initialVelocity = 2f;
+            rb.velocity = Vector2.down * initialVelocity
+
+        }
+
+        //Bullet initialVelocity and acceleration
+        if (directionList == DirectionList.BulletDirection)
+        {
+            acceleration = 200f;
+            initialVelocity = 5f;
+            rb.velocity = Vector2.up * initialVelocity
+
+        }
+
+        //Does nothing due to it coming from PlayerInput
+        if (directionList == DirectionList.PlayerDirection)
+        {
+
+        }
+    }
+
+    private void Update()
+    {
+        //Enemy movement
+        if (directionList == DirectionList.EnemyDirection)
+        {
+            Vector2 EnemyForceToAdd = Vector2.down * acceleration * Time.deltaTime;
+            rb.AddForce(EnemyForceToAdd);
+        }
+        //Bullet Movement
+        if (directionList == DirectionList.BulletDirection)
+        {
+            Vector2 BulletForceToAdd = Vector2.up * acceleration * Time.deltaTime;
+            rb.AddForce(BulletForceToAdd);
+        }
+        //Player Movement
+        public void ObjectMovement(Vector2 direction, float acceleration)
+    {
+        Vector2 Movement = direction * acceleration * Time.deltaTime;
+        rb.AddForce(Movement);
+    }
+}   
